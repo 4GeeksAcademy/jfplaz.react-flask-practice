16094db6ca685036b746463c40a69a8f3063244c
Jesus F Plaz <jfplaz@gmail.com>
2025-02-20 14:11:52 +0000

    First Commit

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -9,11 +10,39 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorites: Mapped[List["Favorite"]] = relationship()
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    favorites: Mapped[List["Favorite"]] = relationship()
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+    
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+
+
+    def serialize(self):
+        planet = db.session.execute(db.select(Planets).filter_by(id=self.planet_id)).scalar_one()
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet": planet.serialize()
+   
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Favorite
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
@@ -12,8 +12,6 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
-
-
 @api.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -22,4 +20,53 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# @api.route('/signup', methods=['POST'])
+# def signup():
+
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
+
+@api.route('/signup/', methods=['POST'])
+def signup():
+    try:
+        request_body=request.json
+        user = db.session.execute(db.select(User).filter_by(email=request_body["email"])).scalar_one()
+    
+        return jsonify({"msg":"user exist"}), 401
+    except:
+        user = User(email=request_body["email"], password=request_body["password"],is_active=request_body["is_active"])
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"msg":"created"}), 201
+   
+
+@api.route("/login", methods=["POST"])
+def login():
+
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = db.session.execute(db.select(User).filter_by(email=email)).scalar_one()
+
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
+
+@api.route("/favorite", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    user = db.session.execute(db.Select(User).filter_by(email=current_user)).scalar_one()
+    favorites = db.session.execute(db.select(Favorite).filter_by(user_id=user.id)).scalars()
+    results = list(map(lambda item: item.serialize(),favorites))
+    print(results)
+    return jsonify({"results": results}), 200
 
